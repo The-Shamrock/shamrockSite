a4a3138bc2ec396078f925e841fb4ed3dcf892b2
jmfcomo <43357248+jmfcomo@users.noreply.github.com>
2024-08-20 14:24:14 -0500

    chore: created ui and pages libraries

---
+++ libs/pages/src/lib/pages/pages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-pages',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pages.component.html',
+  styleUrl: './pages.component.css',
+})
+export class PagesComponent {}
