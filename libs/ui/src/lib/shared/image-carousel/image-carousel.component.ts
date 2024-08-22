564bb3d663008f7ecffd45d51f8e1c472b10cf90
jmfcomo <43357248+jmfcomo@users.noreply.github.com>
2024-08-22 11:18:04 -0500

    chore: added image carousel component

---
+++ libs/ui/src/lib/shared/image-carousel/image-carousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-image-carousel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './image-carousel.component.html',
+  styleUrl: './image-carousel.component.css',
+})
+export class ImageCarouselComponent {}
