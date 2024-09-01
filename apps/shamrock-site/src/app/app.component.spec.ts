70e7872d020bbf7f3a8752c2318f17081c65fe83
jmfcomo <43357248+jmfcomo@users.noreply.github.com>
2024-08-31 21:31:00 -0500

    fix: lint fix

--- apps/shamrock-site/src/app/app.component.spec.ts
+++ apps/shamrock-site/src/app/app.component.spec.ts
@@ -8,19 +8,4 @@ describe('AppComponent', () => {
       imports: [AppComponent, RouterModule.forRoot([])],
     }).compileComponents();
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome shamrock-site'
-    );
-  });
-
-  it(`should have as title 'shamrock-site'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('shamrock-site');
-  });
 });
