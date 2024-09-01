40100090884ca47abe1e37145f168cb61a0625c9
jmfcomo <43357248+jmfcomo@users.noreply.github.com>
2024-08-31 20:17:48 -0500

    chore: created about component

---
+++ libs/ui/src/lib/home/about-section/about-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-about-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './about-section.component.html',
+  styleUrl: './about-section.component.css',
+})
+export class AboutSectionComponent {}
