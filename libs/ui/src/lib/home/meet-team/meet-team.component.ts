f44a9c89b36586aab182aff8c2cf2a8b8bd4004c
jmfcomo <43357248+jmfcomo@users.noreply.github.com>
2024-08-31 21:14:51 -0500

    feat: set up the meet-team structure

--- libs/ui/src/lib/home/meet-team/meet-team.component.ts
+++ libs/ui/src/lib/home/meet-team/meet-team.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MeetTeamPersonComponent } from '.././meet-team-person/meet-team-person.component';
 
 @Component({
   selector: 'lib-meet-team',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MeetTeamPersonComponent],
   templateUrl: './meet-team.component.html',
   styleUrl: './meet-team.component.scss',
 })
