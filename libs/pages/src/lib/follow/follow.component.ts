939dbeb247925ec98cd021fee4c458c38aad529a
jmfcomo <43357248+jmfcomo@users.noreply.github.com>
2024-09-02 20:41:35 -0500

    chore: created follow page component

---
+++ libs/pages/src/lib/follow/follow.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-follow',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './follow.component.html',
+  styleUrl: './follow.component.css',
+})
+export class FollowComponent {}
