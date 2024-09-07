4465caadcaff4ecdfdb25c413b0c56b1f64590cf
jmfcomo <43357248+jmfcomo@users.noreply.github.com>
2024-09-07 15:24:20 -0500

    chore: created watch page component

---
+++ libs/pages/src/lib/watch/watch.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-watch',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './watch.component.html',
+  styleUrl: './watch.component.scss',
+})
+export class WatchComponent {}
