0cc6aee2986363abc7b6c851e730ae947c22fa8e
milli-s <millishousec@gmail.com>
2024-09-19 16:28:07 -0500

    chore: made follow-us component

---
+++ libs/ui/src/lib/follow/follow-us/follow-us.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-follow-us',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './follow-us.component.html',
+  styleUrl: './follow-us.component.css',
+})
+export class FollowUsComponent {}
