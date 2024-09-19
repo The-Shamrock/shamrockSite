372ffdfd9a72e67b68307240626f69276a51e32a
milli-s <millishousec@gmail.com>
2024-09-19 16:35:08 -0500

    chore: create follow-icon component

---
+++ libs/ui/src/lib/follow/follow-icon/follow-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-follow-icon',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './follow-icon.component.html',
+  styleUrl: './follow-icon.component.scss',
+})
+export class FollowIconComponent {}
