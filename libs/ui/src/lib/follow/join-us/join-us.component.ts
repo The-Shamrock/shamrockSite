90cb10d433708ee96033610bd4a120c74f8b1f2d
milli-s <millishousec@gmail.com>
2024-09-19 16:33:51 -0500

    chore: create join-us component

---
+++ libs/ui/src/lib/follow/join-us/join-us.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-join-us',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './join-us.component.html',
+  styleUrl: './join-us.component.scss',
+})
+export class JoinUsComponent {}
