9d2ea9ef7090212fdaf463fb2e3e927408435257
jmfcomo <43357248+jmfcomo@users.noreply.github.com>
2024-09-24 11:56:26 -0500

    chore: created read component

---
+++ libs/pages/src/lib/read/read.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-read',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './read.component.html',
+  styleUrl: './read.component.scss',
+})
+export class ReadComponent {}
