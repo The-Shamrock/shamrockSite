a57585c579af4568fd314333415cad6800203fa7
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2024-10-17 12:06:00 -0500

    wip: adding issue display

--- libs/ui/src/lib/read/issue-sidebar/issue-sidebar.component.ts
+++ libs/ui/src/lib/read/issue-sidebar/issue-sidebar.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+export interface Issue {
+  title: string;
+  imageUrl: string;
+}
 
 @Component({
   selector: 'lib-issue-sidebar',
@@ -8,4 +13,9 @@ import { CommonModule } from '@angular/common';
   templateUrl: './issue-sidebar.component.html',
   styleUrl: './issue-sidebar.component.scss',
 })
-export class IssueSidebarComponent {}
+export class IssueSidebarComponent {
+  @Input() data: Issue = {
+    title: "",
+    imageUrl: ""
+  };
+}
--- libs/ui/src/lib/read/issues-sidebar/issues-sidebar.component.ts
+++ libs/ui/src/lib/read/issues-sidebar/issues-sidebar.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IssueSidebarComponent } from '../issue-sidebar/issue-sidebar.component';
 
 @Component({
   selector: 'lib-issues-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IssueSidebarComponent],
   templateUrl: './issues-sidebar.component.html',
   styleUrl: './issues-sidebar.component.scss',
 })
-export class IssuesSidebarComponent {}
+
+export class IssuesSidebarComponent {
+  issues = [
+    {
+      title: 'Issue 1',
+      imageUrl: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Issue 2',
+      imageUrl: 'https://via.placeholder.com/150'
+    },
+    {
+      title: 'Issue 3',
+      imageUrl: 'https://via.placeholder.com/150'
+    }
+  ];
+}
