a98e9a97c7cb18b842724031e673ee31505023a3
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2024-10-24 16:17:43 -0500

    feat: issue changes display now

--- libs/ui/src/lib/read/issue-reader/issue-reader.component.ts
+++ libs/ui/src/lib/read/issue-reader/issue-reader.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReadStore } from '../store/read.store';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'lib-issue-reader',
@@ -10,12 +11,16 @@ import { ReadStore } from '../store/read.store';
   styleUrl: './issue-reader.component.scss',
 })
 export class IssueReaderComponent {
-  private store = inject(ReadStore);
+  store = inject(ReadStore);
   selectedIssue = './assets/issues/oct2024.pdf';
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
     this.selectedIssue = this.store.selectedIssue();
     console.log("Reader has issue " + this.selectedIssue);
+  }
+
+  getSanitizedUrl(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
 }
