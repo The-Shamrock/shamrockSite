8099baff3ee4e0da286f7bbcce117709140d280c
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2025-01-01 09:43:06 -0600

    feat: made join us component

--- libs/pages/src/lib/follow/follow.component.ts
+++ libs/pages/src/lib/follow/follow.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FollowUsComponent, EmailListComponent } from '@shamrock/ui';
+import { FollowUsComponent, EmailListComponent, JoinUsComponent } from '@shamrock/ui';
 
 @Component({
   selector: 'lib-follow',
   standalone: true,
-  imports: [CommonModule, FollowUsComponent, EmailListComponent],
+  imports: [CommonModule, FollowUsComponent, EmailListComponent, JoinUsComponent],
   templateUrl: './follow.component.html',
   styleUrl: './follow.component.scss',
 })
--- libs/ui/src/lib/follow/join-us/join-us.component.ts
+++ libs/ui/src/lib/follow/join-us/join-us.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FollowIconComponent } from '../follow-icon/follow-icon.component';
+import { followIcon } from '../follow-icon/follow-icon.component';
 
 @Component({
   selector: 'lib-join-us',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FollowIconComponent],
   templateUrl: './join-us.component.html',
   styleUrl: './join-us.component.scss',
 })
-export class JoinUsComponent {}
+export class JoinUsComponent {
+  iconData: followIcon[] = [
+    {
+      imageUrl: "./assets/follow/discordIcon.png",
+      altText: "Discord",
+      name: "Discord",
+      linkUrl: "https://discord.com/invite/caHv9bne6e"
+    },
+    {
+      imageUrl: "./assets/follow/muEngageIcon.jpg",
+      altText: "MU Engage",
+      name: "MU Engage",
+      linkUrl: "https://engage.missouri.edu/TheShamrock/club_signup"
+    }
+  ]
+}
