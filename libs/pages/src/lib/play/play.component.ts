658157f6a691b2483216456ffc55cea5c068cfe7
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2025-01-11 11:57:26 -0600

    feat: added play components into feature page

--- libs/pages/src/lib/play/play.component.ts
+++ libs/pages/src/lib/play/play.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PuzzleSidebarComponent, PuzzleDisplayComponent } from '@shamrock/ui';
 
 @Component({
   selector: 'lib-play',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PuzzleDisplayComponent, PuzzleSidebarComponent],
   templateUrl: './play.component.html',
   styleUrl: './play.component.scss',
 })
