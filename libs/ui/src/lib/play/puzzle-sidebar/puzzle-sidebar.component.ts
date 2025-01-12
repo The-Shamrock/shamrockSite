2d24ee97cbc7f8facbf3f1c338c9d33e6dcf0433
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2025-01-12 16:15:47 -0600

    chore: fixed ng-bootstrap to be working

--- libs/ui/src/lib/play/puzzle-sidebar/puzzle-sidebar.component.ts
+++ libs/ui/src/lib/play/puzzle-sidebar/puzzle-sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'lib-puzzle-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbAccordionModule],
   templateUrl: './puzzle-sidebar.component.html',
   styleUrl: './puzzle-sidebar.component.scss',
 })
