08f496999f8f2ed9717b5c239a1e5af4b770cd8c
Joan Ferguson <joan.ferg@outlook.com>
2025-01-28 13:42:58 -0600

    chore: connected in watch components together

--- libs/pages/src/lib/watch/watch.component.ts
+++ libs/pages/src/lib/watch/watch.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VideoPlayerComponent, VideoCardsComponent, VideoInfoComponent } from '@shamrock/ui';
 
 @Component({
   selector: 'lib-watch',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, VideoCardsComponent, VideoPlayerComponent, VideoInfoComponent],
   templateUrl: './watch.component.html',
   styleUrl: './watch.component.scss',
 })
