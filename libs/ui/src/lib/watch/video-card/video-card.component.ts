2f9612e91135371a259c933c153a0276ea3bdfab
Joan Ferguson <joan.ferg@outlook.com>
2025-01-30 13:46:33 -0600

    feat: added video cards

--- libs/ui/src/lib/watch/video-card/video-card.component.ts
+++ libs/ui/src/lib/watch/video-card/video-card.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VideoCard } from '../video-cards/video-cards.component';
 
 @Component({
   selector: 'lib-video-card',
@@ -7,4 +8,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './video-card.component.html',
   styleUrl: './video-card.component.scss',
 })
-export class VideoCardComponent {}
+export class VideoCardComponent {
+  @Input() data: VideoCard = {img: "", title: ""};
+}
--- libs/ui/src/lib/watch/video-cards/video-cards.component.ts
+++ libs/ui/src/lib/watch/video-cards/video-cards.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VideoCardComponent } from '../video-card/video-card.component';
+
+export interface VideoCard {
+  img: string;
+  title: string;
+}
 
 @Component({
   selector: 'lib-video-cards',
-  imports: [CommonModule],
+  imports: [CommonModule, VideoCardComponent],
   templateUrl: './video-cards.component.html',
   styleUrl: './video-cards.component.scss',
 })
-export class VideoCardsComponent {}
+export class VideoCardsComponent {
+  videoCards: VideoCard[] = [
+    {img: "./assets/watch/thumbnails/greenBurger.png", title: "Green Burger"},
+    {img: "./assets/watch/thumbnails/greenBurger.png", title: "Green Burger"},
+    {img: "./assets/watch/thumbnails/greenBurger.png", title: "Green Burger"}
+  ]
+}
